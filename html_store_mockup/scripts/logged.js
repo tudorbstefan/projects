a422ca603c4474f9900b178014b419adb56e83b1
bod <bodenlosztudor123@gmail.com>
2020-05-08 18:27:13 +0300

    removed folder

--- html_store_mockup/scripts/logged.js
+++
@@ -1,6 +0,0 @@
-function verifyLogState() {
-    if(sessionStorage.getItem('logged')){
-        document.getElementById('login-text').innerHTML = sessionStorage.getItem('username');
-        document.getElementById('login-text').removeAttribute('href');
-    }
-}
